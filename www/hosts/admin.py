d8d3735089667c43fd4359fa72e2b2bfabe24735
opera443399 <pc@pcswo.com>
2016-12-13 17:40:03 +0800

    enhanced admin layout

--- www/hosts/admin.py
+++ www/hosts/admin.py
@@ -27,10 +27,36 @@ class EndUserAdmin(admin.ModelAdmin):
     search_fields = ['username']
 
 class HostAdmin(admin.ModelAdmin):
-    search_fields = ['hostname']
+    fieldsets = [
+        (None, {'fields': ['hostname']}),
+        ('Device', {'fields': ['device_sn', 'device_vendor', 'device_type', 'device_ipmi_ip', 'device_ipmi_user', 'device_ipmi_pass', 'device_raid_level']}),
+        ('IDC Info', {'fields':['idc_info', 'idc_rack', 'idc_rack_h']}),
+        ('OS', {'fields':['os_ip_wan1', 'os_ip_wan2', 'os_ip_lan_mgmt', 'os_ip_lan_stor', 'os_ip_lan_biz', 'os_type', 'os_user_root', 'os_pass_root', 'os_user', 'os_pass']}),
+        ('Status', {'fields':['is_monited', 'is_online', 'is_v_host']}),
+        ('APP', {'fields':['app_desc', 'end_user']}),
+        ('Asset', {'fields':['asset_no']}),
+        ('Date information', {'fields': ['dt_created', 'dt_destroyed'], 'classes': ['collapse']}),
+    ]
+    list_display = ('hostname', 'idc_info', 'os_ip_wan1', 'os_ip_lan_mgmt', 'os_type', 'app_desc', 'end_user',
+                    'device_type', 'device_ipmi_ip', 'device_raid_level',
+                    'is_monited', 'is_online', 'is_v_host', 'was_added_recently')
+    list_filter = ['device_type', 'device_raid_level', 'idc_info', 'os_type', 'end_user', 'dt_created']
+    search_fields = ['hostname', 'device_ipmi_ip', 'os_ip_wan1', 'os_ip_lan_mgmt', 'app_desc']
+
 
 class VmAdmin(admin.ModelAdmin):
     search_fields = ['hostname']
+    fieldsets = [
+        (None, {'fields': ['hostname','on_host']}),
+        ('OS', {'fields': ['os_ip_wan', 'os_ip_lan', 'os_type', 'os_user_root', 'os_pass_root', 'os_user', 'os_pass']}),
+        ('Status', {'fields':['is_monited', 'is_online']}),
+        ('APP', {'fields':['app_desc', 'end_user', 'mount_point', 'desc']}),
+        ('Date information', {'fields': ['dt_created', 'dt_destroyed'], 'classes': ['collapse']}),
+    ]
+    list_display = ('hostname', 'on_host', 'os_ip_wan', 'os_ip_lan', 'os_type', 'app_desc', 'end_user',
+                    'is_monited', 'is_online', 'was_added_recently')    
+    list_filter = ['on_host', 'os_type', 'end_user', 'dt_created']
+    search_fields = ['hostname', 'os_ip_wan', 'os_ip_lan', 'app_desc']
 
 
 admin.site.register(Vendor, VendorAdmin)
--- www/hosts/models.py
+++ www/hosts/models.py
@@ -10,6 +10,8 @@ from django.db import models
 from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
 
+import datetime
+
 # Create your models here.
 
 
@@ -61,61 +63,82 @@ class EndUser(models.Model):
 class Host(models.Model):
     hostname = models.CharField(_('hostname'), max_length=100, unique=True)
     #
-    device_sn = models.CharField(_('Device SN'), max_length=40, null=True, blank=True)
+    device_sn = models.CharField(_('SN'), max_length=40, null=True, blank=True)
     device_vendor = models.ForeignKey(Vendor)
     device_type = models.ForeignKey(DeviceType)
-    device_ipmi_ip = models.GenericIPAddressField(_('Device IPMI IP'), null=True, blank=True)
-    device_ipmi_user = models.CharField(_('Device IPMI User'), max_length=20, null=True, blank=True)
-    device_ipmi_pass = models.CharField(_('Device IPMI Password'), max_length=40, null=True, blank=True)
-    device_raid_level = models.CharField(_('Device RAID Level'), max_length=20, null=True, blank=True)
-    #
-    asset_no = models.CharField(_('Asset No.'), max_length=100, null=True, blank=True)
-    asset_date_in = models.DateTimeField(_('Asset Date In'), auto_now_add=True)
-    asset_date_out = models.DateTimeField(_('Asset Date Out'), auto_now=True)
+    device_ipmi_ip = models.GenericIPAddressField(_('IPMI IP'), null=True, blank=True)
+    device_ipmi_user = models.CharField(_('IPMI User'), max_length=20, null=True, blank=True)
+    device_ipmi_pass = models.CharField(_('IPMI Password'), max_length=40, null=True, blank=True)
+    device_raid_level = models.CharField(_('RAID'), max_length=20, null=True, blank=True)
     #
     idc_info = models.ForeignKey(IDCInfo)
     idc_rack= models.IntegerField(_('IDC Rack No.'), default=0)
     idc_rack_h= models.IntegerField(_('IDC Rack Height'), default=0)
     #
-    os_ip_wan1 = models.GenericIPAddressField(_('OS IP WAN1'), null=True, blank=True)
-    os_ip_wan2 = models.GenericIPAddressField(_('OS IP WAN1'), null=True, blank=True)
-    os_ip_lan_mgmt = models.GenericIPAddressField(_('OS IP LAN MGMT'))
-    os_ip_lan_stor = models.GenericIPAddressField(_('OS IP LAN STOR'), null=True, blank=True)
-    os_ip_lan_biz = models.GenericIPAddressField(_('OS IP LAN BIZ'), null=True, blank=True)
+    os_ip_wan1 = models.GenericIPAddressField(_('IP_WAN1'), null=True, blank=True)
+    os_ip_wan2 = models.GenericIPAddressField(_('IP_WAN2'), null=True, blank=True)
+    os_ip_lan_mgmt = models.GenericIPAddressField(_('IP_LAN_MGMT'))
+    os_ip_lan_stor = models.GenericIPAddressField(_('IP_LAN_STOR'), null=True, blank=True)
+    os_ip_lan_biz = models.GenericIPAddressField(_('IP_LAN_BIZ'), null=True, blank=True)
     os_type = models.ForeignKey(OSType)
-    os_user_root = models.CharField(_('OS root'), max_length=20)
-    os_pass_root = models.CharField(_('Password of root'), max_length=40)
-    os_user = models.CharField(_('OS User'), max_length=20, null=True, blank=True)
+    os_user_root = models.CharField(_('Admin'), max_length=20)
+    os_pass_root = models.CharField(_('Password of Admin'), max_length=40)
+    os_user = models.CharField(_('User'), max_length=20, null=True, blank=True)
     os_pass = models.CharField(_('Password of User'), max_length=40, null=True, blank=True)
     #
-    is_v_host = models.BooleanField(_('Is Virtualization Host?'), default=False)
     is_monited = models.BooleanField(_('Is Monited?'), default=False)
+    is_online = models.BooleanField(_('Is Online?'), default=False)
+    is_v_host = models.BooleanField(_('Is Virtualization Host?'), default=False)
     #
     app_desc = models.CharField(_('App Description'), max_length=40, default='offline')
     end_user = models.ForeignKey(EndUser)
+    #
+    asset_no = models.CharField(_('Asset No.'), max_length=100, null=True, blank=True)
+    dt_created = models.DateTimeField(_('When Created?'))
+    dt_destroyed = models.DateTimeField(_('When Distroyed?'), null=True, blank=True)
     
     def __str__(self):
         return self.hostname
+
+    def was_added_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.dt_created <= now
+
+    was_added_recently.admin_order_field = 'dt_created'
+    was_added_recently.boolean = True
+    was_added_recently.short_description = _('Added recently?')
 
 
 class Vm(models.Model):
     hostname = models.CharField(_('hostname'), max_length=100, unique=True)
     on_host = models.ForeignKey(Host)
     #
-    os_ip_wan = models.GenericIPAddressField(_('OS IP WAN'), null=True, blank=True)
-    os_ip_lan = models.GenericIPAddressField(_('OS IP WAN'))
+    os_ip_wan = models.GenericIPAddressField(_('IP_WAN'), null=True, blank=True)
+    os_ip_lan = models.GenericIPAddressField(_('IP_LAN'))
     os_type = models.ForeignKey(OSType)
-    os_user_root = models.CharField(_('OS root'), max_length=20)
-    os_pass_root = models.CharField(_('Password of root'), max_length=40)
-    os_user = models.CharField(_('OS User'), max_length=20, null=True, blank=True)
+    os_user_root = models.CharField(_('Admin'), max_length=20)
+    os_pass_root = models.CharField(_('Password of Admin'), max_length=40)
+    os_user = models.CharField(_('User'), max_length=20, null=True, blank=True)
     os_pass = models.CharField(_('Password of User'), max_length=40, null=True, blank=True)
     #
     is_monited = models.BooleanField(_('Is Monited?'), default=False)
+    is_online = models.BooleanField(_('Is Online?'), default=False)
     #
     app_desc = models.CharField(_('App Description'), max_length=40, default='offline')
     end_user = models.ForeignKey(EndUser)
     mount_point = models.CharField(_('Mount Point'), max_length=40, null=True, blank=True)
     desc = models.CharField(_('VM Description'), max_length=100, default='Extra info.')
+    #
+    dt_created = models.DateTimeField(_('When Created?'))
+    dt_destroyed = models.DateTimeField(_('When Destoryed?'), null=True, blank=True)
 
     def __str__(self):
         return self.hostname
+
+
+    def was_added_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.dt_created <= now
+    was_added_recently.admin_order_field = 'dt_created'
+    was_added_recently.boolean = True
+    was_added_recently.short_description = _('Added recently?')
