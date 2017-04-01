855fd193b973e2744327038c746dee7b1b774750
opera443399 <pc@pcswo.com>
2017-04-01 18:43:00 +0800

    checkbox added for action: "select all"
    checkbox added for action: "select all" , this action is under todo list
    for bulk-update

--- www/static/assets/js/assets-base.js
+++ www/static/assets/js/assets-base.js
@@ -1,4 +1,8 @@
-/* base */
+/*
+* @desc: base
+* @modified: 2017/4/1
+* */
+
 $(document).ready(function(){
     $("#search").click(function(){
         $("#form-searching").submit();
@@ -6,4 +10,11 @@ $(document).ready(function(){
     $("#select-language").change(function(){
         $("#form-language").submit();
     });
+    $("#checkbox_all").click(function () {
+        $("input[name='checkbox_item']").prop('checked', this.checked)
+    });
+    $("input[name='checkbox_item']").click(function(){
+        $("#checkbox_all").prop("checked",$("input[name='checkbox_item']").length == $("input[name='checkbox_item']:checked").length);
+    });
 });
+
